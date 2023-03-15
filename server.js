38236537a90a8d3dfcc179aa7749c3d5de13e4c1
Vincent Fernandez <fernandez97vincent@gmail.com>
2023-03-15 15:51:52 -0700

    create posts

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const mongoose = require("mongoose")
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+mongoose
+.connect("mongodb://localhost:27017/testDB")
+.catch((err) => console.log(err));
+
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", (req, res) => {
     res.send("Express is here");
+})
+
+app.post("/create", (req, res) => {
+    Post.create({
+        title: req.body.title,
+        description: req.body.description
+    }).then(doc => console.log(doc))
+    .catch(err => console.log(err))
 })
 
 app.listen(3001, function() {
